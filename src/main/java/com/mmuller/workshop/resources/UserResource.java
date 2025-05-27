8e83788d13b8f6415877ea39cc33c95927d65244
Matheus Müller <MM-Muller@users.noreply.github.com>
2025-05-27 16:14:05 -0300

    Entity e REST, criados e funcionando corretamente.

--- src/main/java/com/mmuller/workshop/resources/UserResource.java
+++ src/main/java/com/mmuller/workshop/resources/UserResource.java
@@ -1,4 +1,24 @@
 package com.mmuller.workshop.resources;
 
+import com.mmuller.workshop.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        User matheus = new User("1", "Matheus Muller", "m@m.com");
+        User nalu = new User("2", "Nalu Muller", "n@n.com");
+        List<User> list = new ArrayList<>(Arrays.asList(matheus, nalu));
+        return ResponseEntity.ok().body(list);
+    }
 }
