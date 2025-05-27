b7f49033407f903927603ca8b93f7be4094dcaac
Matheus Müller <MM-Muller@users.noreply.github.com>
2025-05-27 16:55:24 -0300

    Atualizando arquivos e conectando ao MongoDB com repository e service

--- src/main/java/com/mmuller/workshop/resources/UserResource.java
+++ src/main/java/com/mmuller/workshop/resources/UserResource.java
@@ -1,24 +1,27 @@
 package com.mmuller.workshop.resources;
 
 import com.mmuller.workshop.domain.User;
+import com.mmuller.workshop.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService Service;
+    @Autowired
+    private UserService userService;
+
 
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User matheus = new User("1", "Matheus Muller", "m@m.com");
-        User nalu = new User("2", "Nalu Muller", "n@n.com");
-        List<User> list = new ArrayList<>(Arrays.asList(matheus, nalu));
+        List<User> list = userService.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
