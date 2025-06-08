388b28a2e0764395ebce7f9ca49fad61c4bc370d
Matheus Müller <MM-Muller@users.noreply.github.com>
2025-06-08 15:46:05 -0300

    Atualizaçao do usuário com PUT

--- src/main/java/com/mmuller/workshop/resources/UserResource.java
+++ src/main/java/com/mmuller/workshop/resources/UserResource.java
@@ -45,4 +45,12 @@ public class UserResource {
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = userService.fromDTO(objDto);
+        obj.setId(id);
+        obj = userService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
 }
