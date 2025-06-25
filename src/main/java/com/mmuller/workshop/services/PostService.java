c7d0d16b28523169d047f9be32fc9c4e8071127f
Matheus Müller <MM-Muller@users.noreply.github.com>
2025-06-25 17:32:59 -0300

    Obtendo um post por id

---
+++ src/main/java/com/mmuller/workshop/services/PostService.java
@@ -0,0 +1,22 @@
+package com.mmuller.workshop.services;
+
+import com.mmuller.workshop.domain.Post;
+import com.mmuller.workshop.repository.PostRepository;
+import com.mmuller.workshop.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
