e7e793c8afd376cc4b7b6221403d97bfa70ccfd2
Matheus Müller <MM-Muller@users.noreply.github.com>
2025-06-25 16:46:48 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/mmuller/workshop/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.mmuller.workshop.repository;
+
+import com.mmuller.workshop.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
