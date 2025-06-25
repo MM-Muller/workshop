d7422675a41b57f05f9a607d5240ddbb1c3b8a6e
Matheus Müller <MM-Muller@users.noreply.github.com>
2025-06-25 18:06:47 -0300

    Consulta simples com query methods

--- src/main/java/com/mmuller/workshop/resources/PostResource.java
+++ src/main/java/com/mmuller/workshop/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.mmuller.workshop.resources;
 
 import com.mmuller.workshop.domain.Post;
+import com.mmuller.workshop.resources.util.URL;
 import com.mmuller.workshop.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -20,5 +20,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(URL.decodeParam(text));
+        return ResponseEntity.ok().body(list);
     }
 }
---
+++ src/main/java/com/mmuller/workshop/resources/util/URL.java
@@ -0,0 +1,14 @@
+package com.mmuller.workshop.resources.util;
+
+import java.net.URLDecoder;
+
+public class URL {
+
+    public static String decodeParam(String s) {
+        try {
+            return URLDecoder.decode(s, "UTF-8");
+        } catch (Exception e) {
+            return "";
+        }
+    }
+}
